1338b8082041528557371b288fd63131eeba0c8a
MattDerksen1 <91733866+MattDerksen1@users.noreply.github.com>
2021-10-15 13:20:52 -0500

    Add files via upload

--- Camel Up Final Sim R.R
+++ Camel Up Final Sim R.R
@@ -39,7 +39,7 @@ cat("\nPurple Camel starts at space: ", purple_Position)
 cat("\nBlack Camel starts at space: ", black_Position)
 cat("\nWhite Camel starts at space: ", white_Position)
 
-#Board Game Dice Simulation
+#Board Game Dice Simulation in a while loop
 while (red_Position < 17 && blue_Position < 17 && yellow_Position < 17 && green_Position < 17 && purple_Position < 17) {
   print("\n")
   cat("\nStart of Leg ", nLegs)
@@ -47,201 +47,234 @@ while (red_Position < 17 && blue_Position < 17 && yellow_Position < 17 && green_
   
   leg_Rolls[1]
   if (leg_Rolls[1] == "red") {
-    cat("Red Camel move: ", sample(red_Dice, 1, replace = TRUE))
-    red_Position <- red_Position + sample(red_Dice, 1, replace = TRUE)
+    red_Roll <- sample(red_Dice, 1, replace = TRUE)
+    cat("\nRed Camel move: ", red_Roll)
+    red_Position <- red_Position + red_Roll
     cat("\nRed Camel is on space: ", red_Position)
     
   } else if (leg_Rolls[1] == "blue") {
-    cat("\nBlue Camel move: ", sample(blue_Dice, 1, replace = TRUE))
-    blue_Position <- blue_Position + sample(blue_Dice, 1, replace = TRUE)
+    blue_Roll <- sample(blue_Dice, 1, replace = TRUE)
+    cat("\nBlue Camel move: ", blue_Roll)
+    blue_Position <- blue_Position + blue_Roll
     cat("\nBlue Camel is on space: ", blue_Position)
     
   } else if (leg_Rolls[1] == "yellow") {
-    cat("\nYellow Camel move: ", sample(yellow_Dice, 1, replace = TRUE))
-    yellow_Position <- yellow_Position + sample(yellow_Dice, 1, replace = TRUE)
+    yellow_Roll <- sample(yellow_Dice, 1, replace = TRUE)
+    cat("\nYellow Camel move: ", yellow_Roll)
+    yellow_Position <- yellow_Position + yellow_Roll
     cat("\nYellow Camel is on space: ", yellow_Position)
     
   } else if (leg_Rolls[1] == "green") {
-    cat("\nGreen Camel move: ", sample(green_Dice, 1, replace = TRUE))
-    green_Position <- green_Position + sample(green_Dice, 1, replace = TRUE)
+    green_Roll <- sample(green_Dice, 1, replace = TRUE)
+    cat("\nGreen Camel move: ", green_Roll)
+    green_Position <- green_Position + green_Roll
     cat("\nGreen Camel is on space: ", green_Position)
     
   } else if (leg_Rolls[1] == "purple") {
-    cat("\nPurple Camel move: ", sample(purple_Dice, 1, replace = TRUE))
-    purple_Position <- purple_Position + sample(purple_Dice, 1, replace = TRUE)
+    purple_Roll <- sample(purple_Dice, 1, replace = TRUE)
+    cat("\nPurple Camel move: ", purple_Roll)
+    purple_Position <- purple_Position + purple_Roll
     cat("\nPurple Camel is on space: ", purple_Position)
     
   } else if (leg_Rolls[1] == "black and white") {
-    sample(black_White, 1, replace = TRUE)
-    if (sample(black_White, 1, replace = TRUE) == "Black") {
-      cat("\nBlack Camel move: ", sample(black_Dice, 1, replace = TRUE))
-      black_Position <- black_Position - sample(black_Dice, 1, replace = TRUE)
+    black_white_Roll <- sample(black_White, 1, replace = TRUE)
+    if (black_white_Roll == "Black") {
+      black_Roll <- sample(black_Dice, 1, replace = TRUE)
+      cat("\nBlack Camel move: ", black_Roll)
+      black_Position <- black_Position - black_Roll
       cat("\nBlack Camel is on space: ", black_Position)
-    } else if (sample(black_White, 1, replace = TRUE) == "White"){
-      cat("\nWhite Camel move: ", sample(white_Dice, 1, replace = TRUE))
-      white_Position <- white_Position - sample(white_Dice, 1, replace = TRUE)
+    } else if (black_white_Roll == "White"){
+      white_Roll <- sample(white_Dice, 1, replace = TRUE)
+      cat("\nWhite Camel move: ", white_Roll)
+      white_Position <- white_Position - white_Roll
       cat("\nWhite Camel is on space: ", white_Position)
     }
   }
   
   leg_Rolls[2]
   if (leg_Rolls[2] == "red") {
-    cat("\nRed Camel move: ", sample(red_Dice, 1, replace = TRUE))
-    red_Position <- red_Position + sample(red_Dice, 1, replace = TRUE)
+    red_Roll <- sample(red_Dice, 1, replace = TRUE)
+    cat("\nRed Camel move: ", red_Roll)
+    red_Position <- red_Position + red_Roll
     cat("\nRed Camel is on space: ", red_Position)
     
   } else if (leg_Rolls[2] == "blue") {
-    cat("\nBlue Camel move: ", sample(blue_Dice, 1, replace = TRUE))
-    blue_Position <- blue_Position + sample(blue_Dice, 1, replace = TRUE)
+    blue_Roll <- sample(blue_Dice, 1, replace = TRUE)
+    cat("\nBlue Camel move: ", blue_Roll)
+    blue_Position <- blue_Position + blue_Roll
     cat("\nBlue Camel is on space: ", blue_Position)
     
   } else if (leg_Rolls[2] == "yellow") {
-    cat("\nYellow Camel move: ", sample(yellow_Dice, 1, replace = TRUE))
-    yellow_Position <- yellow_Position + sample(yellow_Dice, 1, replace = TRUE)
+    yellow_Roll <- sample(yellow_Dice, 1, replace = TRUE)
+    cat("\nYellow Camel move: ", yellow_Roll)
+    yellow_Position <- yellow_Position + yellow_Roll
     cat("\nYellow Camel is on space: ", yellow_Position)
     
   } else if (leg_Rolls[2] == "green") {
-    cat("\nGreen Camel move: ", sample(green_Dice, 1, replace = TRUE))
-    green_Position <- green_Position + sample(green_Dice, 1, replace = TRUE)
+    green_Roll <- sample(green_Dice, 1, replace = TRUE)
+    cat("\nGreen Camel move: ", green_Roll)
+    green_Position <- green_Position + green_Roll
     cat("\nGreen Camel is on space: ", green_Position)
     
   } else if (leg_Rolls[2] == "purple") {
-    cat("\nPurple Camel move: ", sample(purple_Dice, 1, replace = TRUE))
-    purple_Position <- purple_Position + sample(purple_Dice, 1, replace = TRUE)
+    purple_Roll <- sample(purple_Dice, 1, replace = TRUE)
+    cat("\nPurple Camel move: ", purple_Roll)
+    purple_Position <- purple_Position + purple_Roll
     cat("\nPurple Camel is on space: ", purple_Position)
     
   } else if (leg_Rolls[2] == "black and white") {
-    sample(black_White, 1, replace = TRUE)
-    if (sample(black_White, 1, replace = TRUE) == "Black") {
-      cat("\nBlack Camel move: ", sample(black_Dice, 1, replace = TRUE))
-      black_Position <- black_Position - sample(black_Dice, 1, replace = TRUE)
+    black_white_Roll <- sample(black_White, 1, replace = TRUE)
+    if (black_white_Roll == "Black") {
+      black_Roll <- sample(black_Dice, 1, replace = TRUE)
+      cat("\nBlack Camel move: ", black_Roll)
+      black_Position <- black_Position - black_Roll
       cat("\nBlack Camel is on space: ", black_Position)
-    } else if (sample(black_White, 1, replace = TRUE) == "White") {
-      cat("\nWhite Camel move: ", sample(white_Dice, 1, replace = TRUE))
-      white_Position <- white_Position - sample(white_Dice, 1, replace = TRUE)
+    } else if (black_white_Roll == "White"){
+      white_Roll <- sample(white_Dice, 1, replace = TRUE)
+      cat("\nWhite Camel move: ", white_Roll)
+      white_Position <- white_Position - white_Roll
       cat("\nWhite Camel is on space: ", white_Position)
     }
   }
   
   leg_Rolls[3]
   if (leg_Rolls[3] == "red") {
-    cat("\nRed Camel move: ", sample(red_Dice, 1, replace = TRUE))
-    red_Position <- red_Position + sample(red_Dice, 1, replace = TRUE)
+    red_Roll <- sample(red_Dice, 1, replace = TRUE)
+    cat("\nRed Camel move: ", red_Roll)
+    red_Position <- red_Position + red_Roll
     cat("\nRed Camel is on space: ", red_Position)
     
   } else if (leg_Rolls[3] == "blue") {
-    cat("\nBlue Camel move: ", sample(blue_Dice, 1, replace = TRUE))
-    blue_Position <- blue_Position + sample(blue_Dice, 1, replace = TRUE)
+    blue_Roll <- sample(blue_Dice, 1, replace = TRUE)
+    cat("\nBlue Camel move: ", blue_Roll)
+    blue_Position <- blue_Position + blue_Roll
     cat("\nBlue Camel is on space: ", blue_Position)
     
   } else if (leg_Rolls[3] == "yellow") {
-    cat("\nYellow Camel move: ", sample(yellow_Dice, 1, replace = TRUE))
-    yellow_Position <- yellow_Position + sample(yellow_Dice, 1, replace = TRUE)
+    yellow_Roll <- sample(yellow_Dice, 1, replace = TRUE)
+    cat("\nYellow Camel move: ", yellow_Roll)
+    yellow_Position <- yellow_Position + yellow_Roll
     cat("\nYellow Camel is on space: ", yellow_Position)
     
   } else if (leg_Rolls[3] == "green") {
-    cat("\nGreen Camel move: ", sample(green_Dice, 1, replace = TRUE))
-    green_Position <- green_Position + sample(green_Dice, 1, replace = TRUE)
+    green_Roll <- sample(green_Dice, 1, replace = TRUE)
+    cat("\nGreen Camel move: ", green_Roll)
+    green_Position <- green_Position + green_Roll
     cat("\nGreen Camel is on space: ", green_Position)
     
   } else if (leg_Rolls[3] == "purple") {
-    cat("\nPurple Camel move: ", sample(purple_Dice, 1, replace = TRUE))
-    purple_Position <- purple_Position + sample(purple_Dice, 1, replace = TRUE)
+    purple_Roll <- sample(purple_Dice, 1, replace = TRUE)
+    cat("\nPurple Camel move: ", purple_Roll)
+    purple_Position <- purple_Position + purple_Roll
     cat("\nPurple Camel is on space: ", purple_Position)
     
   } else if (leg_Rolls[3] == "black and white") {
-    sample(black_White, 1, replace = TRUE)
-    if (sample(black_White, 1, replace = TRUE) == "Black") {
-      cat("\nBlack Camel move: ", sample(black_Dice, 1, replace = TRUE))
-      black_Position <- black_Position - sample(black_Dice, 1, replace = TRUE)
+    black_white_Roll <- sample(black_White, 1, replace = TRUE)
+    if (black_white_Roll == "Black") {
+      black_Roll <- sample(black_Dice, 1, replace = TRUE)
+      cat("\nBlack Camel move: ", black_Roll)
+      black_Position <- black_Position - black_Roll
       cat("\nBlack Camel is on space: ", black_Position)
-    } else if (sample(black_White, 1, replace = TRUE) == "White") {
-      cat("\nWhite Camel move: ", sample(white_Dice, 1, replace = TRUE))
-      white_Position <- white_Position - sample(white_Dice, 1, replace = TRUE)
+    } else if (black_white_Roll == "White"){
+      white_Roll <- sample(white_Dice, 1, replace = TRUE)
+      cat("\nWhite Camel move: ", white_Roll)
+      white_Position <- white_Position - white_Roll
       cat("\nWhite Camel is on space: ", white_Position)
     }
   }
   
   leg_Rolls[4]
   if (leg_Rolls[4] == "red") {
-    cat("\nRed Camel move: ", sample(red_Dice, 1, replace = TRUE))
-    red_Position <- red_Position + sample(red_Dice, 1, replace = TRUE)
+    red_Roll <- sample(red_Dice, 1, replace = TRUE)
+    cat("\nRed Camel move: ", red_Roll)
+    red_Position <- red_Position + red_Roll
     cat("\nRed Camel is on space: ", red_Position)
     
   } else if (leg_Rolls[4] == "blue") {
-    cat("\nBlue Camel move: ", sample(blue_Dice, 1, replace = TRUE))
-    blue_Position <- blue_Position + sample(blue_Dice, 1, replace = TRUE)
+    blue_Roll <- sample(blue_Dice, 1, replace = TRUE)
+    cat("\nBlue Camel move: ", blue_Roll)
+    blue_Position <- blue_Position + blue_Roll
     cat("\nBlue Camel is on space: ", blue_Position)
     
   } else if (leg_Rolls[4] == "yellow") {
-    cat("\nYellow Camel move: ", sample(yellow_Dice, 1, replace = TRUE))
-    yellow_Position <- yellow_Position + sample(yellow_Dice, 1, replace = TRUE)
+    yellow_Roll <- sample(yellow_Dice, 1, replace = TRUE)
+    cat("\nYellow Camel move: ", yellow_Roll)
+    yellow_Position <- yellow_Position + yellow_Roll
     cat("\nYellow Camel is on space: ", yellow_Position)
     
   } else if (leg_Rolls[4] == "green") {
-    cat("\nGreen Camel move: ", sample(green_Dice, 1, replace = TRUE))
-    green_Position <- green_Position + sample(green_Dice, 1, replace = TRUE)
+    green_Roll <- sample(green_Dice, 1, replace = TRUE)
+    cat("\nGreen Camel move: ", green_Roll)
+    green_Position <- green_Position + green_Roll
     cat("\nGreen Camel is on space: ", green_Position)
     
   } else if (leg_Rolls[4] == "purple") {
-    cat("\nPurple Camel move: ", sample(purple_Dice, 1, replace = TRUE))
-    purple_Position <- purple_Position + sample(purple_Dice, 1, replace = TRUE)
+    purple_Roll <- sample(purple_Dice, 1, replace = TRUE)
+    cat("\nPurple Camel move: ", purple_Roll)
+    purple_Position <- purple_Position + purple_Roll
     cat("\nPurple Camel is on space: ", purple_Position)
     
   } else if (leg_Rolls[4] == "black and white") {
-    sample(black_White, 1, replace = TRUE)
-    
-    if (sample(black_White, 1, replace = TRUE) == "Black") {
-      cat("\nBlack Camel move: ", sample(black_Dice, 1, replace = TRUE))
-      black_Position <- black_Position - sample(black_Dice, 1, replace = TRUE)
+    black_white_Roll <- sample(black_White, 1, replace = TRUE)
+    if (black_white_Roll == "Black") {
+      black_Roll <- sample(black_Dice, 1, replace = TRUE)
+      cat("\nBlack Camel move: ", black_Roll)
+      black_Position <- black_Position - black_Roll
       cat("\nBlack Camel is on space: ", black_Position)
-    } else if (sample(black_White, 1, replace = TRUE) == "White") {
-      cat("\nWhite Camel move: ", sample(white_Dice, 1, replace = TRUE))
-      white_Position <- white_Position - sample(white_Dice, 1, replace = TRUE)
+    } else if (black_white_Roll == "White"){
+      white_Roll <- sample(white_Dice, 1, replace = TRUE)
+      cat("\nWhite Camel move: ", white_Roll)
+      white_Position <- white_Position - white_Roll
       cat("\nWhite Camel is on space: ", white_Position)
     }
   }
   
   leg_Rolls[5]
   if (leg_Rolls[5] == "red") {
-    cat("\nRed Camel move: ", sample(red_Dice, 1, replace = TRUE))
-    red_Position <- red_Position + sample(red_Dice, 1, replace = TRUE)
+    red_Roll <- sample(red_Dice, 1, replace = TRUE)
+    cat("\nRed Camel move: ", red_Roll)
+    red_Position <- red_Position + red_Roll
     cat("\nRed Camel is on space: ", red_Position)
     
   } else if (leg_Rolls[5] == "blue") {
-    cat("\nBlue Camel move: ", sample(blue_Dice, 1, replace = TRUE))
-    blue_Position <- blue_Position + sample(blue_Dice, 1, replace = TRUE)
+    blue_Roll <- sample(blue_Dice, 1, replace = TRUE)
+    cat("\nBlue Camel move: ", blue_Roll)
+    blue_Position <- blue_Position + blue_Roll
     cat("\nBlue Camel is on space: ", blue_Position)
     
   } else if (leg_Rolls[5] == "yellow") {
-    cat("\nYellow Camel move: ", sample(yellow_Dice, 1, replace = TRUE))
-    yellow_Position <- yellow_Position + sample(yellow_Dice, 1, replace = TRUE)
+    yellow_Roll <- sample(yellow_Dice, 1, replace = TRUE)
+    cat("\nYellow Camel move: ", yellow_Roll)
+    yellow_Position <- yellow_Position + yellow_Roll
     cat("\nYellow Camel is on space: ", yellow_Position)
     
   } else if (leg_Rolls[5] == "green") {
-    cat("\nGreen Camel move: ", sample(green_Dice, 1, replace = TRUE))
-    green_Position <- green_Position + sample(green_Dice, 1, replace = TRUE)
+    green_Roll <- sample(green_Dice, 1, replace = TRUE)
+    cat("\nGreen Camel move: ", green_Roll)
+    green_Position <- green_Position + green_Roll
     cat("\nGreen Camel is on space: ", green_Position)
     
   } else if (leg_Rolls[5] == "purple") {
-    cat("\nPurple Camel move: ", sample(purple_Dice, 1, replace = TRUE))
-    purple_Position <- purple_Position + sample(purple_Dice, 1, replace = TRUE)
+    purple_Roll <- sample(purple_Dice, 1, replace = TRUE)
+    cat("\nPurple Camel move: ", purple_Roll)
+    purple_Position <- purple_Position + purple_Roll
     cat("\nPurple Camel is on space: ", purple_Position)
     
   } else if (leg_Rolls[5] == "black and white") {
-    sample(black_White, 1, replace = TRUE)
-    
-    if (sample(black_White, 1, replace = TRUE) == "Black") {
-      cat("\nBlack Camel move: ", sample(black_Dice, 1, replace = TRUE))
-      black_Position <- black_Position - sample(black_Dice, 1, replace = TRUE)
+    black_white_Roll <- sample(black_White, 1, replace = TRUE)
+    if (black_white_Roll == "Black") {
+      black_Roll <- sample(black_Dice, 1, replace = TRUE)
+      cat("\nBlack Camel move: ", black_Roll)
+      black_Position <- black_Position - black_Roll
       cat("\nBlack Camel is on space: ", black_Position)
-    } else if (sample(black_White, 1, replace = TRUE) == "White") {
-      cat("\nWhite Camel move: ", sample(white_Dice, 1, replace = TRUE))
-      white_Position <- white_Position - sample(white_Dice, 1, replace = TRUE)
+    } else if (black_white_Roll == "White"){
+      white_Roll <- sample(white_Dice, 1, replace = TRUE)
+      cat("\nWhite Camel move: ", white_Roll)
+      white_Position <- white_Position - white_Roll
       cat("\nWhite Camel is on space: ", white_Position)
     }
   }
-  
+
   cat("\nEnd of Leg ", nLegs)
   print("\n")
   cat("\nHere are the Camel positions after Leg ", nLegs)
@@ -251,16 +284,22 @@ while (red_Position < 17 && blue_Position < 17 && yellow_Position < 17 && green_
   cat("\nYellow Camel: ", yellow_Position)
   cat("\nGreen Camel: ", green_Position)
   cat("\nPurple Camel: ", purple_Position)
+  cat("\nBlack Camel: ", black_Position)
+  cat("\nWhite Camel: ", white_Position)
   
   nLegs = nLegs + 1
+  leg_Rolls <- sample(total_Dice, 5, replace = FALSE)
 }
 
+#Final positions of all Camels
 print("Here are the final positions of each camel/n")
 cat("Red Camel: ", red_Position)
 cat("\nBlue Camel: ", blue_Position)
 cat("\nYellow Camel: ", yellow_Position)
 cat("\nGreen Camel: ", green_Position)
 cat("\nPurple Camel: ", purple_Position)
+cat("\nBlack Camel: ", black_Position)
+cat("\nWhite Camel: ", white_Position)
 
 final_Position <- c(red_Position, blue_Position, yellow_Position, green_Position, purple_Position)
 final_Rankings <- order(-final_Position)
@@ -269,4 +308,3 @@ final_Rankings
 camelUp_Sim
 
 replicate(B, camelUp_Sim)
-
